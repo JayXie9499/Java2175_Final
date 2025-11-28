876717be13d67d37c2c6eecffcfe13b0f0d48759
JayXie9499 <tsum950210@gmail.com>
2025-11-29 00:58:21 +0800

    feat: Implement database initialization and table creation

--- src/com/mybank/service/Database.java
+++ src/com/mybank/service/Database.java
@@ -1,4 +1,38 @@
 package com.mybank.service;
-import java.sql.Connection;
+
+import java.sql.*;
+
 public class Database {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:sqlite:data.db");
+    }
+
+    public static void init() {
+        try {
+            final Connection conn = getConnection();
+            final Statement stmt = conn.createStatement();
+            stmt.executeUpdate("""
+                    CREATE TABLE banks IF NOT EXISTS (
+                        id TEXT UNIQUE NOT NULL,
+                        name TEXT UNIQUE NOT NULL,
+                        transferFee REAL NOT NULL,
+                        exchangeFee REAL NOT NULL
+                    );
+                    """);
+            stmt.executeUpdate("""
+                    CREATE TABLE accounts IF NOT EXISTS (
+                        id TEXT UNIQUE NOT NULL,
+                        name TEXT NOT NULL,
+                        hashedPwd TEXT NOT NULL,
+                        role INTEGER DEFAULT 1,
+                        balance INTEGER DEFAULT 0,
+                        bankId TEXT NOT NULL,
+                    
+                        FOREIGN KEY (bankId) REFERENCES banks(id)
+                    );
+                    """);
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+    }
 }
